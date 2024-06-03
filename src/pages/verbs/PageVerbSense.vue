71db98f2645e750d2ac9e6712840943cc98c6ee4
vodish <vodish@yandex.ru>
2024-06-03 20:49:08 +0300

    upd

--- src/pages/verbs/PageVerbSense.vue
+++ src/pages/verbs/PageVerbSense.vue
@@ -1,10 +1,68 @@
-<script setup lang="ts">
-// sdvsd
-</script>
-
 <template>
   <p>
     Кроме передачи смысла, с&nbsp;помощью глагола мы&nbsp;передаем время действия. <br />
     Нагрузка временем изменяет форму глагола, он&nbsp;по-другому пишется.
   </p>
+
+  <div class="col1 frame1">
+    <div class="hidden">
+      <div class="border subject"></div>
+    </div>
+    <div>
+      <div class="border">ожидать</div>
+    </div>
+    <div>
+      <div class="note">
+        просто глагол (начальная форма), <br />
+        ничего не обозначает.
+      </div>
+    </div>
+  </div>
+  <div class="col1 frame1">
+    <div>
+      <div class="border subject select">Я</div>
+    </div>
+    <div>
+      <div class="border">ожида<u class="time">ю</u>.</div>
+    </div>
+    <div>
+      <div class="note">
+        это уже предложение;<br />
+        здесь глагол меняет форму, <br />
+        указывает настоящее <u class="time">время</u> (сейчас).
+      </div>
+    </div>
+  </div>
+  <div class="col1 frame1">
+    <div>
+      <div class="border subject select">Мы</div>
+    </div>
+    <div>
+      <div class="border">ожида<u class="time">ем</u>.</div>
+    </div>
+    <div>
+      <div class="note">
+        тоже предложение;<br />
+        на форму глагола влияет также <u class="sel">субъект</u>.
+      </div>
+    </div>
+  </div>
+
+
+  ожидать
+  просто глагол,начальная форма
+  Это уже предложение из двух слов.
+  ожидаю
+  здесь глагол меняет форму показывая время,он показывает настоящее время (сейчас).
+  Мы
+  ожидаем
+  на форму глагола влияет также субъект.
+
 </template>
+
+
+<style scoped>
+.hidden {
+  opacity: 0;
+}
+</style>
