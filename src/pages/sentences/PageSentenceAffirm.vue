3aab5e94698fe56170d5bde8de6ed9d27c476708
pkarasev <p.karasev@psw.ru>
2024-06-04 14:27:35 +0300

    upd

--- src/pages/sentences/PageSentenceAffirm.vue
+++ src/pages/sentences/PageSentenceAffirm.vue
@@ -1,18 +1,112 @@
 <script setup lang="ts">
-// sdvsd
+const subject = ['Я', 'Вы', 'Мы', 'Они', 'Он', 'Она', 'Это']
 </script>
 
 <template>
   <p>
-    Утвердительное предложение в&nbsp;английском языке, точно также как и&nbsp;в&nbsp;русском, состоит из&nbsp;субъекта
-    и&nbsp;действия. <span class="time">Время</span> действия передается через форму смыслового глагола или через модальный глагол. Обратите
-    внимание что смысловой глагол (<u class="sel">expect = ожидать</u>) имеет всего три формы, в&nbsp;отличии от&nbsp;русского языка
-    (я&nbsp;ожидаю, он&nbsp;ожидал и&nbsp;тд.)
+    <b>Утвердительное предложение</b> в&nbsp;английском языке, точно также как и&nbsp;в&nbsp;русском,
+    состоит из&nbsp;<u class="sel">субъекта</u> и&nbsp;действия.
+    <span class="time">Время</span> действия передается через форму смыслового глагола или через
+    модальный глагол. Обратите внимание что смысловой глагол <u class="sel">expect</u> (ожидать) имеет всего три формы,
+    в&nbsp;отличии от&nbsp;русского языка (я&nbsp;ожидаю, вы&nbsp;ожидаете и&nbsp;тд.)
 
   </p>
+
+
+  <div class="col1 frame1">
+    <div>
+      <div class="border">
+        <table>
+          <tr v-for="subj in subject" :key="subj">
+            <td>{{ subj }}</td>
+            <td>expect<u class="time">ed</u></td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (я, вы, мы, они...); <br />
+        форма смыслового глагола;
+        <div>Прошедшее время (было)</div>
+      </div>
+    </div>
+    <div>
+      <div class="border">
+        <table>
+          <tr>
+            <td>Я</td>
+            <td><u class="time">expect</u>.</td>
+            <td class="rus">(ожидаю)</td>
+          </tr>
+          <tr>
+            <td>Вы</td>
+            <td><u class="time">expect</u>.</td>
+            <td class="rus">(ожидаeте)</td>
+          </tr>
+          <tr>
+            <td>Мы</td>
+            <td><u class="time">expect</u>.</td>
+            <td class="rus">(ожидаем)</td>
+          </tr>
+          <tr>
+            <td>Они</td>
+            <td><u class="time">expect</u>.</td>
+            <td class="rus">(ожидают)</td>
+          </tr>
+          <tr>
+            <td>Он</td>
+            <td>expect<u class="time">s</u>.</td>
+            <td class="rus">(ожидает)</td>
+          </tr>
+          <tr>
+            <td>Она</td>
+            <td>expect<u class="time">s</u>.</td>
+            <td class="rus">(ожидает)</td>
+          </tr>
+          <tr>
+            <td>Это</td>
+            <td>expect<u class="time">s</u>.</td>
+            <td class="rus">(ожидает)</td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (он, она, это); <br />
+        форма смыслового глагола;
+        <div>Настоящее время (сейчас)</div>
+      </div>
+    </div>
+    <div>
+
+      <div class="border">
+        <table>
+          <tr v-for="subj in subject" :key="subj">
+            <td>{{ subj }}</td>
+            <td class="time">will</td>
+            <td>expect.</td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (это); <br />
+        форма модального глагола; <br />
+        <p>Будущее время (будет)</p>
+      </div>
+    </div>
+  </div>
+
   <p>
     Тонкий момент&nbsp;&mdash; в&nbsp;настоящем времени начальная форма смыслового глагола является одновременно
-    указателем настоящего времени. Поэтому в&nbsp;таблице подсвечено полностью все слово, т.к. в&nbsp;предложении вместе
-    с&nbsp;субъектом оно указывает настоящее время.
+    указателем настоящего времени. Поэтому в&nbsp;таблице подсвечено полностью все слово <u class="time">expect</u>, т.к.
+    рядом с&nbsp;субъектом оно указывает настоящее время.
   </p>
 </template>
+
+<style scoped>
+.border {
+  min-width: 210px;
+  width: fit-content !important;
+}
+.rus {
+  font-size: 0.8em;
+}
+</style>
--- src/pages/sentences/PageSentenceAux.vue
+++ src/pages/sentences/PageSentenceAux.vue
@@ -1,19 +1,111 @@
 <script setup lang="ts">
-// sdvsd
+const subject = ['Я', 'Вы', 'Мы', 'Они', 'Он', 'Она', 'Это']
 </script>
+
 
 <template>
   <p>
-    В&nbsp;английском языке глаголы делятся на&nbsp;два типа:
+    В&nbsp;английском языке <b>глаголы делятся на&nbsp;два типа</b>:
   <ul>
-    <li>правильные (в&nbsp;прошедшем времени добавляется окончание &laquo;ed&raquo;, а&nbsp;в&nbsp;настоящем
-      &laquo;s&raquo;)</li>
-    <li>неправильные (которые имеют собственные формы вместо правильных окончаний)</li>
+    <li>правильные (в&nbsp;прошедшем времени добавляется окончание "<u class="time">ed</u>", а&nbsp;в&nbsp;настоящем
+      "<b class="time">s</b>")</li>
+    <li>неправильные (которые имеют собственные формы вместо окончаний по-правилам)</li>
   </ul>
   </p>
   <p>
-    Смысловой глагол&nbsp;<b>do</b> (делать) &mdash; неправильный.
+    Смысловой глагол&nbsp;<u class="sel">do</u> (делать) &mdash; неправильный.
     Ещё у него есть особая роль: он&nbsp;является вспомогательным глаголом и употребляется в&nbsp;отрицательных предложениях.
     Когда глагол do&nbsp;появляется в&nbsp;предложении как вспомогательный то&nbsp;он&nbsp;никак не&nbsp;переводится.
   </p>
+
+  <div class="col1 frame1">
+    <div>
+      <div class="border">
+        <table>
+          <tr v-for="subj in subject" :key="subj">
+            <td>{{ subj }}</td>
+            <td><u class="time">did</u></td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (я, вы, мы, они...); <br />
+        форма смыслового глагола;
+        <div>Прошедшее время (было)</div>
+      </div>
+    </div>
+    <div>
+      <div class="border">
+        <table>
+          <tr>
+            <td>Я</td>
+            <td><u class="time">do</u>.</td>
+            <td class="rus">(делал)</td>
+          </tr>
+          <tr>
+            <td>Вы</td>
+            <td><u class="time">do</u>.</td>
+            <td class="rus">(делали)</td>
+          </tr>
+          <tr>
+            <td>Мы</td>
+            <td><u class="time">do</u>.</td>
+            <td class="rus">(делали)</td>
+          </tr>
+          <tr>
+            <td>Они</td>
+            <td><u class="time">do</u>.</td>
+            <td class="rus">(делали)</td>
+          </tr>
+          <tr>
+            <td>Он</td>
+            <td><u class="time">does</u>.</td>
+            <td class="rus">(делал)</td>
+          </tr>
+          <tr>
+            <td>Она</td>
+            <td><u class="time">does</u>.</td>
+            <td class="rus">(делала)</td>
+          </tr>
+          <tr>
+            <td>Это</td>
+            <td><u class="time">does</u>.</td>
+            <td class="rus">(делало)</td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (он, она, это); <br />
+        форма смыслового глагола;
+        <div>Настоящее время (сейчас)</div>
+      </div>
+    </div>
+    <div>
+      
+      <div class="border">
+        <table>
+          <tr v-for="subj in subject" :key="subj">
+            <td>{{ subj }}</td>
+            <td class="time">will</td>
+            <td>do.</td>
+          </tr>
+        </table>
+      </div>
+      <div class="note">
+        субъект (это); <br />
+        форма модального глагола; <br />
+        <p>Будущее время (будет)</p>
+      </div>
+    </div>
+  </div>
+
 </template>
+
+<style scoped>
+.border {
+  width: 210px !important;
+}
+.rus {
+  font-size: 0.8em;
+}
+</style>
