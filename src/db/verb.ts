427d056f26b8aceb8164915c0cf317bae1621541
pkarasev <p.karasev@psw.ru>
2024-06-06 15:18:28 +0300

    upd

---
+++ src/db/verb.ts
@@ -0,0 +1,9 @@
+const verb = {
+    do: import('./verb/verb.do'),
+    be: import('./verb/verb.be'),
+    expect: import('./verb/verb.expect'),
+}
+
+export default verb;
+
+
---
+++ src/db/verb/verb.be.ts
@@ -0,0 +1,45 @@
+export default {
+  'ru infinitive': 'быть',
+  
+  'ru I presentSimple': 'буду',
+  'ru you presentSimple': 'будете',
+  'ru we presentSimple': 'будем',
+  'ru they presentSimple': 'будут',
+  'ru he presentSimple': 'будет',
+  'ru she presentSimple': 'будет',
+  'ru it presentSimple': 'будет',
+
+  'ru I pastSimple': 'был',
+  'ru you pastSimple': 'были',
+  'ru we pastSimple': 'были',
+  'ru they pastSimple': 'были',
+  'ru he pastSimple': 'был',
+  'ru she pastSimple': 'была',
+  'ru it pastSimple': 'было',
+  
+  'ru I futureSimple': 'буду',
+  'ru you futureSimple': 'будете',
+  'ru we futureSimple': 'будем',
+  'ru they futureSimple': 'будут',
+  'ru he futureSimple': 'будет',
+  'ru she futureSimple': 'будет',
+  'ru it futureSimple': 'будет',
+  
+  'en infinitive': 'be',
+  
+  'en I presentSimple': 'am',
+  'en you presentSimple': 'are',
+  'en we presentSimple': 'are',
+  'en they presentSimple': 'are',
+  'en he presentSimple': 'is',
+  'en she presentSimple': 'is',
+  'en it presentSimple': 'is',
+
+  'en I pastSimple': 'was',
+  'en you pastSimple': 'were',
+  'en we pastSimple': 'were',
+  'en they pastSimple': 'were',
+  'en he pastSimple': 'was',
+  'en she pastSimple': 'was',
+  'en it pastSimple': 'was',
+};
