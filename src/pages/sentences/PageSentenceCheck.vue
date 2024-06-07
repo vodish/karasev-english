59c5f92cf1e2504bdc05ac24b79fc6fc14ed7c2e
pkarasev <p.karasev@psw.ru>
2024-06-07 15:42:58 +0300

    upd

--- src/pages/sentences/PageSentenceCheck.vue
+++ src/pages/sentences/PageSentenceCheck.vue
@@ -15,52 +15,13 @@ const enPresentSimple = ['expect', 'expect', 'expect', 'expect', 'expects', 'exp
 </script>
 
 <template>
-  
-  <table>
-    <thead>
-      <td colspan="2">Past simple</td>
-      <td class="subj">Subject</td>
-      <td colspan="2">Present simple</td>
-    </thead>
-    <tbody>
-      <tr v-for="(subj, i) in subject" :key="subj + route.params.verb">
-        <td>
-          <div contenteditable="plaintext-only" :tabindex="10 + i">{{ enPastSimple[i] }}</div>
-        </td>
-        <td>
-          <div contenteditable="plaintext-only" :tabindex="20 + i">ожидал</div>
-        </td>
-        <td class="subj">{{ subj }}</td>
-        <td>
-          <div contenteditable="plaintext-only" :tabindex="30 + i">{{ enPresentSimple[i] }}</div>
-        </td>
-        <td>
-          <div contenteditable="plaintext-only" :tabindex="40 + i">ожидаю</div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+
+  <h3>Показать таблицу предложений с глаголами.</h3>
+  <h3>Все варианты для проверки: Время, Предлложение, Субъект</h3>
+
 </template>
 
 
 <style scoped>
-thead > td {
-  font-weight: bold;
-}
-td {
-  vertical-align: baseline;
-  padding: 10px 10px 0 10px;
-}
-[contenteditable] {
-  /* border-bottom: solid 1px var(--color-border); */
-  border-bottom: solid 1px transparent;
-  padding: 2px;
-}
-[contenteditable]:focus {
-  outline: none;
-  border-bottom-color: #ccc;
-}
 
-.subj {
-  padding: 3px 5ch;
-}</style>
+</style>
