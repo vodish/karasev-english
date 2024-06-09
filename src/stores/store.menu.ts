348fb1e45e889e7f0bc9382be5242bb923a478e4
vodish <vodish@yandex.ru>
2024-06-09 22:06:42 +0300

    upd

--- src/stores/store.menu.ts
+++ src/stores/store.menu.ts
@@ -1,7 +1,6 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { routes } from '../router/routes'
-import { useLink, useRouter, type RouteLocationNormalized } from 'vue-router'
 
 
 const categoryList = new Map
