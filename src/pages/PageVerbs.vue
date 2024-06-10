c75c1dc27544d3e89bd4e3f22de44e2285306438
pkarasev <p.karasev@psw.ru>
2024-06-10 13:15:22 +0300

    upd

--- src/pages/PageVerbs.vue
+++ src/pages/PageVerbs.vue
@@ -4,15 +4,11 @@ import { useRoute } from 'vue-router';
 import type { TpatternKey } from '@/db/db.pattern';
 import { listSentence, listTime, listSubject, patterns, transForm } from '@/stores/store.sentence';
 
-const sentenсeRu : {[k: string]: string} = {'affirmative': 'Утверждение', 'negative': 'Отрицание', 'question': 'Вопрос'}
+const sentenсeRu: { [k: string]: string } = { 'affirmative': 'Утверждение', 'negative': 'Отрицание', 'question': 'Вопрос' }
 
 
 const route = useRoute()
-
-
-
-const verb = computed(()=> route.params.verb)
-
+const verb = computed(() => route.params.verb)
 const form = computed(() => {
   let table: { [k: string]: string } = {}
 
@@ -37,39 +33,39 @@ const form = computed(() => {
 </script>
 
 <template>
-  <p>Проверка тренажёра предложений.</p>
   <div>
     Глаголы:
-    <RouterLink to="/verbs/expect">expect</RouterLink> | 
+    <RouterLink to="/verbs/expect">expect</RouterLink> |
     <RouterLink to="/verbs/do">do</RouterLink>
   </div>
 
-
-  <div class="tbl2" v-if="verb">
-    <div class="top">
-      <div class="time"></div>
-      <div class="sel">Прошeдшее</div>
-      <div class="sel">Настоящее</div>
-      <div class="sel">Будушее</div>
-    </div>
-    <div class="row" v-for="sentence in listSentence" :key="sentence">
-      <div class="sel sentence">{{ sentenсeRu[sentence] }}</div>
-      <div class="time" v-for="time in listTime" :key="time">
-        <div class="subject" v-for="subject in listSubject" :key="subject">
-          <div>
-            <div>{{ form[`${sentence} ${time} ${subject} ruForm`] }}</div>
-            <div>{{ form[`${sentence} ${time} ${subject} enForm`] }}</div>
+  <div v-show="verb">
+    <br><br><br>
+    <h3>Предложения в тренажёре</h3>
+    <div class="tbl2" v-if="verb">
+      <div class="top">
+        <div class="time"></div>
+        <div class="sel">Прошeдшее</div>
+        <div class="sel">Настоящее</div>
+        <div class="sel">Будушее</div>
+      </div>
+      <div class="row" v-for="sentence in listSentence" :key="sentence">
+        <div class="sel sentence">{{ sentenсeRu[sentence] }}</div>
+        <div class="time" v-for="time in listTime" :key="time">
+          <div class="subject" v-for="subject in listSubject" :key="subject">
+            <div>
+              <div>{{ form[`${sentence} ${time} ${subject} ruForm`] }}</div>
+              <div>{{ form[`${sentence} ${time} ${subject} enForm`] }}</div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 
 <style scoped>
-
 .row {
   margin-top: 30px;
 }
