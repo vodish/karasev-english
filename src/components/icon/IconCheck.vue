ca3a87d770a5993db1c7423f3b566ea48068fc02
vodish <vodish@yandex.ru>
2024-06-15 20:19:51 +0300

    upd

---
+++ src/components/icon/IconCheck.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  size: { type: Number, default: 24 },
+})
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size + 'px'" :height="size + 'px'" viewBox="0 -960 960 960">
+    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+  </svg>
+</template>
+
+<style scoped>
+svg > path {
+  fill: #F19E39;
+}
+</style>
--- src/pages/subjects/PageSubjectRus.vue
+++ src/pages/subjects/PageSubjectRus.vue
@@ -1,41 +1,94 @@
 <script setup lang="ts">
-const subject = [
-  { en: 'I', ru: 'я' },
-  { en: 'you', ru: 'ты' },
-  { en: 'we', ru: 'мы' },
-  { en: 'they', ru: 'они' },
-  { en: 'he', ru: 'он' },
-  { en: 'she', ru: 'она' },
-  { en: 'it', ru: 'оно' },
-]
+import { ref } from 'vue'
+import IconStar from '@/components/icon/IconStar.vue'
+import IconWarning from '@/components/icon/IconWarning.vue'
+import IconCheck from '@/components/icon/IconCheck.vue'
+import IconCheckCircle from '@/components/icon/IconCheckCircle.vue'
+
+const subject = ref([
+  { en: 'I', ru: 'я', input: '', check: false },
+  { en: 'you', ru: 'ты', input: '', check: false },
+  { en: 'we', ru: 'мы', input: '', check: false },
+  { en: 'they', ru: 'они', input: '', check: false },
+  { en: 'he', ru: 'он', input: '', check: false },
+  { en: 'she', ru: 'она', input: '', check: false },
+  { en: 'it', ru: 'оно', input: '', check: false },
+])
+
+function check1(value, k) {
+  subject.value[k].input = value
+  const { ru, input } = subject.value[k]
+  subject.value[k].check = ru.toLowerCase() === input.toLowerCase()
+}
+
+function compareStr(srt: string, type: string) {
+  if (type === '') return 'wait'
+
+  const str1 = srt.toLowerCase()
+  const type1 = type.toLowerCase()
+
+  if (str1 === type1) return 'done'
+
+}
+
 </script>
 
 <template>
   <p>
     Кстати, в&nbsp;английском &laquo;я&raquo; всегда пишется с&nbsp;заглавной буквы&nbsp;&mdash; <span
       style="font-family: Times;">I</span>. <br />
-      Впиши субъкта &laquo;по-русски&raquo;, в&nbsp;третью колонку.
+    Впиши субъкта &laquo;по-русски&raquo;, в&nbsp;третью колонку.
   </p>
 
-  <table class="col1 tbl1">
-    <thead>
-      <td>Субъект</td>
-      <td>Перевод</td>
-      <td>По-русски</td>
-      <td>Проверка</td>
-    </thead>
-    <tr>
-      <td></td>
-    </tr>
-    <tbody>
-      <template v-for="({ en, ru }) in subject" :key="ru">
-        <tr>
-          <td>{{ en }}</td>
-          <td>{{ ru }}</td>
-          <td><input type="text" class="subject" maxlength="4" /></td>
-          <td>...</td>
-        </tr>
-      </template>
-    </tbody>
-  </table>
+  <div class="flex">
+    <table class="col1 tbl1">
+      <thead>
+        <td>Субъект</td>
+        <td>Перевод</td>
+        <td>По-русски</td>
+        <td>Проверка</td>
+      </thead>
+      <tr>
+        <td></td>
+      </tr>
+      <tbody>
+        <template v-for="({ en, ru, input, check }, k) in subject" :key="ru">
+          <tr>
+            <td>{{ en }}</td>
+            <td>{{ ru }}</td>
+            <td><input type="text" class="subject" maxlength="3" @input="event => check1(event.target.value, k)" /></td>
+            <td class="check">
+              <span v-if="input === ''">...</span>
+              <IconStar v-else-if="check" />
+              <IconWarning v-else />
+
+              <span>...</span>
+              <IconStar :size="20" />
+              <IconWarning :size="20" />
+
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="done">
+      <IconCheckCircle :size="200" />
+    </div>
+  </div>
 </template>
+
+
+<style scoped>
+td.check {
+  display: flex;
+  align-items: center;
+}
+
+.flex {
+  display: flex;
+  gap: 5%;
+}
+.flex > .done {
+  margin-top: 5em;
+}
+</style>
