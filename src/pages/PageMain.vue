a3dc425dbf3ab6030b63fc55246778a7dae18eea
vodish <vodish@yandex.ru>
2024-07-16 20:49:28 +0300

    upd

--- src/pages/PageMain.vue
+++ src/pages/PageMain.vue
@@ -1,4 +1,25 @@
 <template>
-  <div>Привет!</div>
-  <p>Жми ниже кнопку <b>Дальше</b>.</p>
+  <p>Привет! Это тренажер по&nbsp;английскому, сделан на&nbsp;основе ролика Дмитрия Колесникова с&nbsp;азами английской
+    граматики.
+  </p>
+  <p>
+    Вы&nbsp;должны довести свой навык составления базовых предложений&nbsp;&mdash; до&nbsp;автоматизма. Это значит 
+    вообще не&nbsp;задумываться как построить предложение, всё должно получаться
+    на&nbsp;автомате. Только так вы&nbsp;сможете эффективно двигаться дальше.</p>
+  <br>
+
+  <iframe src="https://www.youtube.com/embed/sFWIiEP4V9w?si=r848OfC2G4GDt4zP&amp;controls=0"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 </template>
+
+<style scoped>
+iframe {
+  display: block;
+  margin: 0 auto;
+  width: 560px;
+  height: 315px;
+}
+</style>
